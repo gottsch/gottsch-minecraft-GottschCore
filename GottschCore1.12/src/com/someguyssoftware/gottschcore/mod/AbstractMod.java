b988c045caa7dce3e69fbe83acdfe0e78b202d1b
gottsch <mark.gottschling@gmail.com>
2017-07-17 13:05:10 -0400

    Added addRollingFileAppenderToLogger() to GottschCore

--- GottschCore1.12/src/com/someguyssoftware/gottschcore/mod/AbstractMod.java
+++ GottschCore1.12/src/com/someguyssoftware/gottschcore/mod/AbstractMod.java
@@ -3,6 +3,18 @@
  */
 package com.someguyssoftware.gottschcore.mod;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
+import org.apache.logging.log4j.core.appender.rolling.SizeBasedTriggeringPolicy;
+import org.apache.logging.log4j.core.config.AppenderRef;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+
+import com.someguyssoftware.gottschcore.config.ILoggerConfig;
 import com.someguyssoftware.gottschcore.eventhandler.LoginEventHandler;
 import com.someguyssoftware.gottschcore.version.BuildVersion;
 import com.someguyssoftware.gottschcore.version.VersionChecker;
@@ -88,4 +100,72 @@ public abstract class AbstractMod implements IMod {
 		AbstractMod.latestVersion = latestVersion;
 	}
 
+	/**
+	 * Add rolling file appender to the current logging system.
+	 */
+	public void addRollingFileAppenderToLogger(String loggerName, String appenderName, ILoggerConfig modConfig) {
+		// get config properties
+		String loggerLevel = modConfig.getLoggerLevel();
+		String loggerFolder = modConfig.getLoggerFolder();
+		
+		if (!loggerFolder.endsWith("/")) {
+			loggerFolder += "/";
+		}
+
+		final LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
+        final Configuration config = loggerContext.getConfiguration();
+        
+        // create a sized-based trigger policy, using config setting for size.
+        SizeBasedTriggeringPolicy policy = SizeBasedTriggeringPolicy.createPolicy(modConfig.getLoggerSize());
+        // create the pattern for log statements
+        PatternLayout layout = PatternLayout
+        		.newBuilder()
+        		.withPattern("%d [%t] %p %c | %F:%L | %m%n")
+        		.withAlwaysWriteExceptions(true)
+        		.build();
+        
+        // create a rolling file appender for SGS_Treasure logger (which is used by the Treasure mod)
+        Appender appender = RollingFileAppender
+        		.newBuilder()
+        		.withFileName(loggerFolder + modConfig.getLoggerFilename() + ".log")
+        		.withFilePattern(loggerFolder + modConfig.getLoggerFilename() + "-%d{yyyy-MM-dd-HH_mm_ss}.log")
+        		.withAppend(true)
+        		.withName(appenderName)
+        		.withBufferedIo(true)
+        		.withImmediateFlush(true)
+        		.withPolicy(policy)
+//        		.withStrategy(strategy)
+        		.withLayout(layout)
+//        		.withFilter(filter)
+        		.withIgnoreExceptions(true)
+        		.withAdvertise(false)
+        		.setConfiguration(config)
+        		.build();
+
+        // start the appender
+        appender.start();
+        
+        // add appenders to config
+        config.addAppender(appender);
+        
+        // create appender references
+        AppenderRef appenderReference = AppenderRef.createAppenderRef(appenderName, null, null);
+        
+        // create logger config
+        AppenderRef[] refs = new AppenderRef[] {appenderReference};
+
+		Level level = Level.getLevel(loggerLevel.toUpperCase());
+		
+        // set the logger name "FastLadder" to use the rolling file appender
+        LoggerConfig loggerConfig = LoggerConfig.createLogger("false", level, loggerName, "true", refs, null, config, null );
+        
+        // add appenders to logger config
+        loggerConfig.addAppender(appender, null, null);
+
+        // add loggers to base configuration
+        config.addLogger(loggerName, loggerConfig);
+        
+        // update existing loggers
+        loggerContext.updateLoggers();	
+	}
 }
