516ed9b7e82c6b189d32bb28999e921f320731b9
gottsch <mark.gottschling@gmail.com>
2017-09-07 23:53:14 -0400

    Initial commit.

---
+++ GottschCore1.10.2/src/com/someguyssoftware/gottschcore/command/ShowVersionCommand.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package com.someguyssoftware.gottschcore.command;
+
+import com.someguyssoftware.gottschcore.GottschCore;
+import com.someguyssoftware.gottschcore.mod.IMod;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+
+/**
+ * 
+ * @author Mark Gottschling on Feb 10, 2017
+ *
+ */
+public class ShowVersionCommand extends CommandBase {
+	private IMod mod;
+	
+	/**
+	 * 
+	 * @param mod
+	 */
+	public ShowVersionCommand(IMod mod) {
+		this.mod = mod;
+	}
+	
+	@Override
+	public void execute(MinecraftServer server, ICommandSender commandSender, String[] args) {
+		EntityPlayer player = (EntityPlayer) commandSender.getCommandSenderEntity();
+		try {
+
+//			if (args[0] == null || args[0].equals("")) player.sendMessage(new TextComponentString("Missing mod ID."));
+			
+//			String modid;
+//			modid =args[0];
+			
+			if (player != null) {
+				player.addChatMessage(new TextComponentString(mod.getName() + " version: " + mod.getVersion()));
+				player.addChatMessage(new TextComponentString("Latest released version: " + mod.getModLatestVersion().toString()));
+    		}
+		}
+		catch(Exception e) {
+			player.addChatMessage(new TextComponentString("Error:  " + e.getMessage()));
+			GottschCore.logger.error("Error :", e);
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+		return true;
+	}
+
+	@Override
+	public String getCommandName() {
+		return "showversion-" + this.mod.getId();
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		return "showversion-" + this.mod.getId();
+	}
+}
