bbbb8fc16a26a8d0b28472a830b4461de744cabe
gottsch <17928819+gottsch@users.noreply.github.com>
2020-01-15 13:06:36 -0500

    Create BasedBlock.java

---
+++ GottschCore1.14.4/src/main/java/com/someguyssoftware/gottschcore/block/BasedBlock.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package com.someguyssoftware.gottschcore.block;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Rotation;
+
+/**
+ * 
+ * @author Mark Gottschling on Jan 15, 2020
+ *
+ */
+public abstract class FacingBlock extends ModBlock implements IBasedBlock {
+	
+	/**
+	 * 
+	 * @param modID
+	 * @param name
+	 * @param materialIn
+	 */
+	public FacingBlock(String modID, String name, Block.Properties properties) {
+		super(modID, name, properties);
+		this.setDefaultState(this.stateContainer.getBaseState().with(BASE, Direction.NORTH));
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(BASE);
+	}
+
+    
+	/**
+	 * 
+	 */
+    @Nullable
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+    	return this.getDefaultState().with(FACING, context.getPlayer().getFacing());
+    }
+    
+	/**
+	 * Returns the blockstate with the given rotation from the passed blockstate. If inapplicable, returns the passed
+	 * blockstate.
+	 * @deprecated call via {@link IBlockState#withRotation(Rotation)} whenever possible. Implementing/overriding is
+	 * fine.
+	 */
+	@Override
+	public BlockState rotate(BlockState state, Rotation rotation) {
+		return state.with(BASE, rotation.rotate(getFacing(state)));
+	}
+	
+	/**
+	 * 
+	 * @param state
+	 * @return
+	 */
+	@Override
+	public Direction getBase(BlockState state) {
+		return state.get(BASE);
+	}
+}
