ed0db3b292558f1c9759d80af47e59687c472833
gottsch <mark.gottschling@gmail.com>
2021-01-13 00:35:20 -0500

    delete 1.12.2 files

--- src/main/java/com/someguyssoftware/gottschcore/property/IPropertyCopier.java
+++
@@ -1,9 +0,0 @@
-package com.someguyssoftware.gottschcore.property;
-
-import net.minecraft.block.state.IBlockState;
-
-public interface IPropertyCopier {
-
-	IBlockState copy(IBlockState source, IBlockState dest);
-
-}
