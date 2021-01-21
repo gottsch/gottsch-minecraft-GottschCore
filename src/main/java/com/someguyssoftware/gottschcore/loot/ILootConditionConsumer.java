7730442367167a860449fd7a7859738c0f384ac1
gottsch <mark.gottschling@gmail.com>
2021-01-21 00:51:36 -0500

    remove deprecated loot classes

--- src/main/java/com/someguyssoftware/gottschcore/loot/ILootConditionConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.someguyssoftware.gottschcore.loot;
-
-import com.someguyssoftware.gottschcore.loot.conditions.ILootCondition;
-@Deprecated
-public interface ILootConditionConsumer<T> {
-   T acceptCondition(ILootCondition.IBuilder conditionBuilder);
-
-   T cast();
-}
--- src/main/java/com/someguyssoftware/gottschcore/loot/ILootFunctionConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.someguyssoftware.gottschcore.loot;
-
-import com.someguyssoftware.gottschcore.loot.functions.ILootFunction;
-@Deprecated
-public interface ILootFunctionConsumer<T> {
-   T acceptFunction(ILootFunction.IBuilder functionBuilder);
-
-   T cast();
-}
