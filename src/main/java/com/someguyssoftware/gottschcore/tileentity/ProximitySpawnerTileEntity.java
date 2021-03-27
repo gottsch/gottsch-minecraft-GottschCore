75457f13295eefc35b415d2cc2c21b3a439ee9e0
gottsch <mark.gottschling@gmail.com>
2021-03-27 11:05:02 -0400

    updated proximity spawner

--- src/main/java/com/someguyssoftware/gottschcore/tileentity/ProximitySpawnerTileEntity.java
+++ src/main/java/com/someguyssoftware/gottschcore/tileentity/ProximitySpawnerTileEntity.java
@@ -3,6 +3,7 @@
  */
 package com.someguyssoftware.gottschcore.tileentity;
 
+import java.util.Optional;
 import java.util.Random;
 
 import com.someguyssoftware.gottschcore.GottschCore;
@@ -13,17 +14,21 @@ import com.someguyssoftware.gottschcore.spatial.ICoords;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StringUtils;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.DungeonHooks;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -68,7 +73,7 @@ public class ProximitySpawnerTileEntity extends AbstractProximityTileEntity {
 				defaultMobSpawnerSettings();
 				return;
 			}
-			
+
 			int min = 1;
 			int max = 1;
 			if (nbt.contains("mobNumMin")) {
@@ -90,55 +95,63 @@ public class ProximitySpawnerTileEntity extends AbstractProximityTileEntity {
 	public CompoundNBT save(CompoundNBT tag) {
 		super.save(tag);
 		if (getMobName() == null || StringUtils.isNullOrEmpty(getMobName().toString())) {        	
-            defaultMobSpawnerSettings();
-        }
+			defaultMobSpawnerSettings();
+		}
 		tag.putString("mobName", getMobName().toString());
 		tag.putInt("mobNumMin", getMobNum().getMinInt());
 		tag.putInt("mobNumMax", getMobNum().getMaxInt());
 		return tag;
 	}
 
-    /**
-     * 
-     */
-    private void defaultMobSpawnerSettings() {
-        setMobName(new ResourceLocation("minecraft", "zombie"));
-        setMobNum(new Quantity(1, 1));
-        setProximity(5.0D);
-    }
-    
+	/**
+	 * 
+	 */
+	private void defaultMobSpawnerSettings() {
+		setMobName(new ResourceLocation("minecraft", "zombie"));
+		setMobNum(new Quantity(1, 1));
+		setProximity(5.0D);
+	}
+
 	/**
 	 * 
 	 */
 	@Override
 	public void execute(World world, Random random, ICoords blockCoords, ICoords playerCoords) {
+		if (world.isClientSide()) {
+			return;
+		}
 
 		int mobCount = RandomHelper.randomInt(random, getMobNum().getMinInt(), getMobNum().getMaxInt());
 
 		for (int i = 0; i < mobCount; i++) {
-			Entity entity = ForgeRegistries.ENTITIES.getValue(getMobName()).create(world);
-			if (entity == null) {
-				GottschCore.LOGGER.debug("unable to create entity -> {}", getMobName());
+			Optional<EntityType<?>> entityType = EntityType.byString(getMobName().toString());
+			if (!entityType.isPresent()) {
+				GottschCore.LOGGER.debug("unable to get entityType -> {}", getMobName());
 				selfDestruct();
 				return;
-			}
+			}			
 
-			// TODO refactor all of this - look at AbstractSpawner
-			entity.moveTo((double) getBlockPos().getX() + 0.5D, (double) getBlockPos().getY(), (double) getBlockPos().getZ() + 0.5D,
-					MathHelper.wrapDegrees(world.random.nextFloat() * 360.0F), 0.0F);
+			double x = getBlockPos().getX() + 0.5D;
+			double y = getBlockPos().getY();
+			double z =getBlockPos().getZ() + 0.5D;
+			
+			if (world.noCollision(entityType.get().getAABB(x, y, z))) {
+				ServerWorld serverWorld = (ServerWorld)world;
+				if (EntitySpawnPlacementRegistry.checkSpawnRules(entityType.get(), serverWorld, SpawnReason.SPAWNER, new BlockPos(x, y, z), world.getRandom())) {
+					Entity entity = entityType.get().create(serverWorld);
+					if (entity == null) {
+						GottschCore.LOGGER.debug("unable to create entity -> {}", getMobName());
+						selfDestruct();
+						return;
+					}
 
-			if (entity instanceof MobEntity) {
-				MobEntity mobEntity = (MobEntity) entity;
+					entity.moveTo(entity.getX(), entity.getY(), entity.getZ(), world.random.nextFloat() * 360.0F, 0.0F);
 
-				if (mobEntity.collidingisColliding(world)) {
-					mobEntity.rotationYawHead = mobEntity.rotationYaw;
-					mobEntity.renderYawOffset = mobEntity.rotationYaw;
-
-					(mobEntity).onInitialSpawn(world, world.getDifficultyForLocation(new BlockPos(entity)),
-							SpawnReason.SPAWNER, (ILivingEntityData) null, (CompoundNBT) null);
-
-					world.addEntity(entity);
-					mobEntity.playAmbientSound();
+					if (!serverWorld.addFreshEntity(entity)) {
+						GottschCore.LOGGER.debug("unable to spawn entity in world -> {}", getMobName());
+						selfDestruct();
+						return;
+					}
 				}
 			}
 		}
@@ -150,10 +163,10 @@ public class ProximitySpawnerTileEntity extends AbstractProximityTileEntity {
 	 * 
 	 */
 	private void selfDestruct() {
-		GottschCore.LOGGER.debug("te self-destructing @ {}", getPos()); 
+		GottschCore.LOGGER.debug("te self-destructing @ {}", getBlockPos()); 
 		this.setDead(true);
-		this.getWorld().setBlockState(getPos(), Blocks.AIR.getDefaultState());
-		this.getWorld().removeTileEntity(getPos());
+		this.getLevel().setBlock(getBlockPos(), Blocks.AIR.defaultBlockState(), 3);
+		this.getLevel().removeBlockEntity(getBlockPos());
 	}
 
 	/**
